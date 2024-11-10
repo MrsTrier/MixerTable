35785342aa0a61c1dcb68448b22a3208328b8aee
Cheremina Daria <d.cheremina@gmail.com>
2024-11-10 18:15:23 +0100

    Initial Commit

---
+++ MixerTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MixerTable
+//
+//  Created by Daria Cheremina on 10/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
